fb0f4439b3074525dc9d81263194426a1c5b6cb7
huy <eresh38111@gmail.com>
2022-01-10 14:28:58 +0700

    Updates

---
+++ precache-manifest.05830095d28a0e4abc37288c889491c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc3e13af51fd06fbafcf9ef96892724",
+    "url": "/saga/index.html"
+  },
+  {
+    "revision": "4c7c8495a52f06233185",
+    "url": "/saga/static/css/main.fcb13871.chunk.css"
+  },
+  {
+    "revision": "de649527a9e193496c8c",
+    "url": "/saga/static/js/2.b8316ae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saga/static/js/2.b8316ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7c8495a52f06233185",
+    "url": "/saga/static/js/main.bac2a64f.chunk.js"
+  },
+  {
+    "revision": "98f8f018c4d61c1613dc",
+    "url": "/saga/static/js/runtime-main.c2a2a9b5.js"
+  },
+  {
+    "revision": "9b2b410b3894da9da9e86fea5832dafa",
+    "url": "/saga/static/media/bg.9b2b410b.png"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/saga/static/media/loading.504b535c.gif"
+  }
+]);
